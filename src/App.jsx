c459f57c6efd5dd27a2c281caa6a751a40afe4c7
hai.dinh <hai.dinh@amela.vn>
2022-05-01 21:48:43 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Portfolio from "./components/Portfolio";
+import Services from "./components/Services";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
